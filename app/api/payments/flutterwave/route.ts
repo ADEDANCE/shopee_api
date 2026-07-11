31a3d5b04b8227a74308946f90aa05ce832e0071
Adedance <uthmanadedance@gmail.com>
2026-07-11 23:59:03 +0100

    fecth amount from firebase

--- app/api/payments/flutterwave/route.ts
+++ app/api/payments/flutterwave/route.ts
@@ -1,35 +1,51 @@
 import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/firebase/admin";
 
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
 
+    const orderDoc = await db.collection("orders").doc(body.orderId).get();
+
+    // Check if the order exists
+
+    if (!orderDoc.exists) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Order not found",
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+
+    // extract order data
+    const orderData = orderDoc.data()!;
 
     // fetch from Flutterwave
-    const response = await fetch(
-      "https://api.flutterwave.com/v3/payments",
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.FLUTTERWAVE_SECRET_KEY}`,
-          "Content-Type": "application/json",
+    const response = await fetch("https://api.flutterwave.com/v3/payments", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.FLUTTERWAVE_SECRET_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        tx_ref: body.orderId,
+        amount: orderData.total,
+        currency: "NGN",
+        redirect_url: "http://localhost:3000/payment-success",
+        customer: {
+          email: body.email,
+          name: body.name,
         },
-        body: JSON.stringify({
-          tx_ref: body.orderId,
-          amount: body.amount,
-          currency: "NGN",
-          redirect_url: "http://localhost:3000/payment-success",
-          customer: {
-            email: body.email,
-            name: body.name,
-          },
-          customizations: {
-            title: "Shopee",
-            description: "Order Payment",
-          },
-        }),
-      }
-    );
+        customizations: {
+          title: "Shopee",
+          description: "Order Payment",
+        },
+      }),
+    });
 
     const data = await response.json();
 
@@ -44,7 +60,7 @@ export async function POST(request: NextRequest) {
       },
       {
         status: 500,
-      }
+      },
     );
   }
 }
